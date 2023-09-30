5bc5e2b2a636b0530b98d6c6d82356173da72df6
Jams Lin <james@cardinalpoints.com.hk>
2023-09-30 19:04:56 +0800

    changing dashboard page logic

--- MVC/app.js
+++ MVC/app.js
@@ -1,59 +1,58 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-const express = require('express');
-const mongoose = require('mongoose');
-const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const session = require('express-session');
-const bcrypt = require('bcryptjs');
-const app = express()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const app = express();
 
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 app.use(
   session({
-    secret: 'secret_key',
+    secret: "secret_key",
     resave: true,
     saveUninitialized: true,
   })
 );
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', '_include/layouts/layout')
-app.use(expressLayouts)
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "_include/layouts/layout");
+app.use(expressLayouts);
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
 
-const username = process.env.DB_USERNAME
-const password = process.env.DB_PASSWORD
-const cluster = process.env.DB_CLUSTER
-const dbname = process.env.DB_NAME
-const mongoDBURL = `mongodb+srv://${username}:${password}@${cluster}/${dbname}?retryWrites=true&w=majority`
-console.log(mongoDBURL)
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const cluster = process.env.DB_CLUSTER;
+const dbname = process.env.DB_NAME;
+const mongoDBURL = `mongodb+srv://${username}:${password}@${cluster}/${dbname}?retryWrites=true&w=majority`;
+console.log(mongoDBURL);
 mongoose.connect(mongoDBURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   session({
-    secret: 'secret_key',
+    secret: "secret_key",
     resave: true,
     saveUninitialized: true,
   })
 );
 
 //setting up routes
-const indexRouter = require('./routes/index')
-app.use('/', indexRouter)
-const dashboardRouter = require('./routes/dashboard')
-app.use('/dashboard', dashboardRouter)
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
+const dashboardRouter = require("./routes/dashboard");
+app.use("/dashboard", dashboardRouter);
 
-app.listen(process.env.PORT || 3000)
-
+app.listen(process.env.PORT || 3000);
--- MVC/routes/dashboard.js
+++ MVC/routes/dashboard.js
@@ -1,111 +1,139 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const session = require('express-session');
-const bodyParser = require('body-parser')
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const CommonModel = require('../models/allData');
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const CommonModel = require("../models/allData");
 
 // Define the authentication middleware function
-router.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+router.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 router.use(
   session({
-    secret: 'secret_key',
+    secret: "secret_key",
     resave: true,
     saveUninitialized: true,
   })
 );
 const authenticateUser = (req, res, next) => {
-if (req.session.user) {
-    console.log('passed')
+  if (req.session.user) {
+    console.log("passed");
     // User is authenticated, continue to the next middleware or route handler
-    next()
-} else {
+    next();
+  } else {
     // User is not authenticated, redirect to the login page
-    console.log("failed")
-    res.redirect('/dashboard/login')
-}
-}
+    console.log("failed");
+    res.redirect("/dashboard/login");
+  }
+};
 
 // Use the authentication middleware for routes that require authentication
-router.get('/', authenticateUser, async(req, res) => {
-    // Render the dashboard page for authenticated users
-    let data
-    try {
-        data = await CommonModel.find()
-      } catch {
-        console.log("failed to get data")
-      }
-    console.log(data)
+router.get("/", authenticateUser, async (req, res) => {
+  // Render the dashboard page for authenticated users
+  let data;
+  try {
+    data = await CommonModel.find();
+  } catch {
+    console.log("failed to get data");
+  }
 
-    res.render('dashboard', { title: "Dashboard", user: req.session.user, data: data})
-})
-
- router
-    .route('/login')
-    .get((req, res) => {
-        res.render('dashboard/login', {title:"Login", error: null})
-    })
-    .post(async (req, res) => {
-    try {
-        const { username, password } = req.body
-        const user = await User.findOne({ username })
-
-        if (!user) {
-            console.log('1');
-
-        return res.render('dashboard/login', {title: "Login", error: 'Invalid username or password.' })
-        }
-
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-
-        if (!isPasswordMatch) {
-            console.log('2');
-
-        return res.render('dashboard/login', {title: "Login", error: 'Invalid username or password.' })
-        }
-
-        req.session.user = {id: user._id, username:  user.username}
-        console.log( user)
-        res.redirect('/dashboard'); // Redirect to the authenticated user's dashboard
-    } catch (error) {
-        console.log('4')
-        res.render('dashboard/login', {title: "Login", error: 'Error logging in. Please try again.' })
-    }
-    })
-
-router
-    .route("/register")
-    .get((req, res) => {
-        res.render('dashboard/register', {title:"register", error: null});
-      })
-    .post(async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-        console.log(req.body)
-        if (existingUser) {
-        return res.render('dashboard/register', { title: 'register', error: 'Username or email already exists.' });
-        }
-    
-        const newUser = new User({ username, email, password })
-        await newUser.save()
-        res.redirect('/dashboard/login')
-    } catch (error) {
-        res.render('dashboard/register', { title: 'register' , error: 'Error registering user. Please try again.' });
-    }
-    })
-
-    
-router
-.route("/logout")
-.get((req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.error('Error logging out:', err);
-    }
-    res.redirect('/dashboard/login');
+  res.render("dashboard", {
+    title: "Dashboard",
+    user: req.session.user,
+    collection: data,
   });
 });
 
-module.exports = router
+router
+  .route("/updateDB")
+  .post(async (req, res) => {
+    console.log(req.body);
+    const responseData = { message: req.body };
+    res.json(responseData);
+  })
+  .get(async (req, res) => {
+    console.log(req.query.field);
+    const responseData = { message: req.query.field };
+    res.json(responseData);
+  });
+
+router
+  .route("/login")
+  .get((req, res) => {
+    res.render("dashboard/login", { title: "Login", error: null });
+  })
+  .post(async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        console.log("1");
+        return res.render("dashboard/login", {
+          title: "Login",
+          error: "Invalid username or password.",
+        });
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordMatch) {
+        console.log("2");
+        return res.render("dashboard/login", {
+          title: "Login",
+          error: "Invalid username or password.",
+        });
+      }
+
+      req.session.user = { id: user._id, username: user.username };
+      console.log(user);
+      res.redirect("/dashboard"); // Redirect to the authenticated user's dashboard
+    } catch (error) {
+      console.log("4");
+      res.render("dashboard/login", {
+        title: "Login",
+        error: "Error logging in. Please try again.",
+      });
+    }
+  });
+
+router
+  .route("/register")
+  .get((req, res) => {
+    res.render("dashboard/register", { title: "register", error: null });
+  })
+  .post(async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const existingUser = await User.findOne({
+        $or: [{ username }, { email }],
+      });
+      console.log(req.body);
+      if (existingUser) {
+        return res.render("dashboard/register", {
+          title: "register",
+          error: "Username or email already exists.",
+        });
+      }
+
+      const newUser = new User({ username, email, password });
+      await newUser.save();
+      res.redirect("/dashboard/login");
+    } catch (error) {
+      res.render("dashboard/register", {
+        title: "register",
+        error: "Error registering user. Please try again.",
+      });
+    }
+  });
+
+router.route("/logout").get((req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error logging out:", err);
+    }
+    res.redirect("/dashboard/login");
+  });
+});
+
+module.exports = router;
